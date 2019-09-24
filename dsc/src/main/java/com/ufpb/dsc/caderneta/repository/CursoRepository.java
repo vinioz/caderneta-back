9260266f125ec2fc6d2dc1baee80e57e682dc256
Gutho <guthyelvis.santos@dce.ufpb.br>
2019-09-24 02:38:51 -0300

    model e repository curso

---
+++ dsc/src/main/java/com/ufpb/dsc/caderneta/repository/CursoRepository.java
@@ -0,0 +1,10 @@
+package com.ufpb.dsc.caderneta.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.models.Curso;
+
+
+public interface CursoRepository extends CrudRepository<Curso,Integer>{
+
+}
