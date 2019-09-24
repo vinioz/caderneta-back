36741b84a8b9f40f6733914f32df5ccc9d70fd3e
Gutho <guthyelvis.santos@dce.ufpb.br>
2019-09-24 02:35:56 -0300

    model e repository professor

---
+++ dsc/src/main/java/com/ufpb/dsc/caderneta/repository/ProfessorRepository.java
@@ -0,0 +1,10 @@
+package com.ufpb.dsc.caderneta.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.models.Professor;
+
+
+public interface ProfessorRepository extends CrudRepository<Professor,Integer>{
+
+}
